906d3da2464d78730f64ed286be5794ee4b16033
James Volmert <james.volmert@gmail.com>
2017-04-04 19:00:27 -0600

    fixing reducer for all blog posts

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+// reach out and grab all of the blog posts from the api
+import axios from 'axios';
+
+export const FETCH_POSTS = 'FETCH_POSTS';
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api;';
+const API_KEY = '?key=8888';
+
+export function fetchPosts() {
+
+    const request = axios.get(`${ROOT_URL}/posts/${API_KEY}`);
+    return {
+        type: FETCH_POSTS,
+        payload: request
+    }
+}
